b83246b3e5034c2aaaaae7394a92a340d541caa7
Николай Ханжин <khanzhin@rado.domen>
2022-04-22 09:19:41 +0400

    deploy 4

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -1,6 +1,6 @@
 import React, { Dispatch, FC, SetStateAction} from "react";
 import ListUsers from './ListUsers';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import {HashRouter as Router,Routes,Route} from "react-router-dom";
 
 import '../styles/content.scss';
 import ProfileUser from "./ProfileUser";
@@ -21,9 +21,9 @@ const ContentContainer: FC<listUsersProps> = ({users,setlistUsers,onChangeInput}
 
                 <Routes>
 
-                    <Route path="/listUser/" element={<ListUsers listUsers={users} />} />
+                    <Route path="/" element={<ListUsers listUsers={users} />} />
 
-                    <Route path='/listUser/moreInfo/:number' element={<ProfileUser onChangeInput={onChangeInput} setlistUsers={setlistUsers} listUsers={users} />} />
+                    <Route path='/moreInfo/:number' element={<ProfileUser onChangeInput={onChangeInput} setlistUsers={setlistUsers} listUsers={users} />} />
 
 
                 </Routes>
