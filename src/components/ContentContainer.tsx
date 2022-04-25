714f1fdf305a452a13d079f985d43d27788758e2
Николай Ханжин <khanzhin@rado.domen>
2022-04-25 14:05:44 +0400

    deploy add page 404

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -18,12 +18,12 @@ const ContentContainer: FC<listUsersProps> = ({ users, setlistUsers, onChangeInp
 
 
         <div className="right-block">
-
+<ListUsers listUsers={users} />
             <Routes>
-                <Route path="/" element={<ListUsers listUsers={users} />} />
+                {/* <Route path={"/"} element={<ListUsers listUsers={users} />} /> */}
 
 
-                <Route path='/moreInfo/:number' element={<ProfileUser onChangeInput={onChangeInput} setlistUsers={setlistUsers} listUsers={users} />} />
+                <Route path={'/moreInfo/:number'} element={<ProfileUser onChangeInput={onChangeInput} setlistUsers={setlistUsers} listUsers={users} />} />
 
 
             </Routes>
