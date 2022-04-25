aac127154e37c1ce540a001d81cfbb0d88e315ea
Николай Ханжин <khanzhin@rado.domen>
2022-04-25 13:33:04 +0400

    deploy87

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter, Routes} from "react-router-dom";
+// import { BrowserRouter, HashRouter, Routes} from "react-router-dom";
+import { HashRouter as Router} from "react-router-dom";
 import App from "./components/App";
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
+    <Router>
         <App />
-    </BrowserRouter>
+    </Router>
 
 )
