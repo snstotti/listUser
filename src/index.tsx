ff8693e296c47e89af55a56373c5fbc40c10fed1
Николай Ханжин <khanzhin@rado.domen>
2022-04-25 13:46:16 +0400

    deploy add page 404

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import { BrowserRouter, HashRouter, Routes} from "react-router-dom";
-import { HashRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import App from "./components/App";
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
         <App />
     </Router>
 
