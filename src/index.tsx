4045b827510d743b704c5b5106d602efb9660093
Николай Ханжин <khanzhin@rado.domen>
2022-04-25 13:28:12 +0400

    deploy8

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <HashRouter>
+    <BrowserRouter>
         <App />
-    </HashRouter>
+    </BrowserRouter>
 
 )
